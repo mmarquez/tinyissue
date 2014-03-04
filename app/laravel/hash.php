2a10d3e387ddcf613fc8b5d028fc2a842894d670
Moisés Márquez <moises.marquez.g@gmail.com>
2014-03-04 17:41:07 +0100

    Updated to use PHP 5.5 Hash API

--- app/laravel/hash.php
+++ app/laravel/hash.php
@@ -3,7 +3,7 @@
 class Hash {
 
 	/**
-	 * Hash a password using the Bcrypt hashing scheme.
+	 * Hash a password using the new API in PHP > 5.5
 	 *
 	 * <code>
 	 *		// Create a Bcrypt hash of a value
@@ -19,23 +19,7 @@ class Hash {
 	 */
 	public static function make($value, $rounds = 8)
 	{
-		$work = str_pad($rounds, 2, '0', STR_PAD_LEFT);
-
-		// Bcrypt expects the salt to be 22 base64 encoded characters including
-		// dots and slashes. We will get rid of the plus signs included in the
-		// base64 data and replace them with dots.
-		if (function_exists('openssl_random_pseudo_bytes'))
-		{
-			$salt = openssl_random_pseudo_bytes(16);
-		}
-		else
-		{
-			$salt = Str::random(40);
-		}
-
-		$salt = substr(strtr(base64_encode($salt), '+', '.'), 0 , 22);
-
-		return crypt($value, '$2a$'.$work.'$'.$salt);
+		return password_hash($value, PASSWORD_DEFAULT);
 	}
 
 	/**
@@ -47,7 +31,7 @@ class Hash {
 	 */
 	public static function check($value, $hash)
 	{
-		return crypt($value, $hash) === $hash;
+		return password_verify($value, $hash);
 	}
 
 }
